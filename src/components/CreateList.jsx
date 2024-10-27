e058cac7f718ee64d2afaf719458dfc9cd799142
Mirwais <mirwais.sanaie@gmail.com>
2024-10-28 00:58:01 +0430

    ui updated.

--- src/components/CreateList.jsx
+++ src/components/CreateList.jsx
@@ -2,7 +2,14 @@ import { useState } from "react";
 import Navigation from "./Navigation";
 
 const myList = [
-  { id: 1, name: "Apple", quantity: 2, price: 1000, checked: false },
+  {
+    id: 1,
+    name: "Apple",
+    quantity: 2,
+    unit: "kg",
+    price: 1000,
+    checked: false,
+  },
   // { id: 2, name: "Banana", quantity: 1, unit: "kg", checked: true },
   // { id: 3, name: "Orange", quantity: 3, unit: "kg", checked: false },
   // { id: 4, name: "Milk", quantity: 1, unit: "L", checked: true },
@@ -11,9 +18,9 @@ const myList = [
 
 export default function CreateList() {
   const [items, setItems] = useState(myList);
-  const [isShow, setIsShow] = useState(false);
   const [name, setName] = useState("");
   const [quantity, setQuantity] = useState("");
+  const [unit, setUnit] = useState("");
   const [price, setPrice] = useState("");
 
   function handleSubmit(e) {
@@ -23,6 +30,7 @@ export default function CreateList() {
     const newItem = {
       name,
       quantity,
+      unit,
       price,
       checked: false,
     };
@@ -36,20 +44,15 @@ export default function CreateList() {
     setPrice(0);
   }
 
-  function handleIsShow(e) {
-    e.preventDefault();
-    setIsShow(() => !isShow);
-  }
-
   function clearList() {
-    setItems([]);
+    window.confirm("Are you sure to clear the list?") && setItems([]);
   }
 
   return (
     <div>
       <Navigation />
       {/* create list section  */}
-      <div className="md:w-1/2 w-4/5 mx-auto py-7 text-center">
+      <div className="md:w-1/2 md:mx-auto mx-2 py-7 text-center">
         <h1 className="font-black text-2xl sm:text-3xl">
           <span className="font-normal">Create</span> Shopping list
         </h1>
@@ -62,33 +65,37 @@ export default function CreateList() {
             <div>
               <List items={items} />
             </div>
-
             {/* input of add item */}
-            {isShow && (
-              <div className="flex mx-3 space-x-2">
-                <input
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  type="text"
-                  className="w-1/3 text-sm sm:text-base px-2 py-2 input-createlist"
-                  placeholder="Item-name"
-                />
-                <input
-                  value={quantity}
-                  onChange={(e) => setQuantity(Number(e.target.value))}
-                  type="number"
-                  className="py-2 w-1/3 input-createlist text-center px-2"
-                  placeholder="Count"
-                />
-                <input
-                  value={price}
-                  onChange={(e) => setPrice(Number(e.target.value))}
-                  type="number"
-                  className="py-2 w-1/3 input-createlist text-center px-2"
-                  placeholder="price"
-                />
-              </div>
-            )}
+            <div className="grid grid-cols-5 mx-3 space-x-2 divide-x-2 divide-dashed divide-gray-400 border border-gray-300">
+              <input
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                type="text"
+                className="text-sm col-span-2 sm:text-base px-2 py-2 input-createlist"
+                placeholder="Item-name"
+              />
+              <input
+                value={quantity}
+                onChange={(e) => setQuantity(Number(e.target.value))}
+                type="number"
+                className="py-2 input-createlist px-2"
+                placeholder="Count"
+              />
+              <input
+                value={unit}
+                onChange={(e) => setUnit(e.target.value)}
+                type="text"
+                className="py-2 input-createlist px-2"
+                placeholder="Unit"
+              />
+              <input
+                value={price}
+                onChange={(e) => setPrice(Number(e.target.value))}
+                type="number"
+                className="py-2 input-createlist px-2"
+                placeholder="price"
+              />
+            </div>
 
             <div className="flex justify-between mx-3">
               <div className="space-x-2">
@@ -99,9 +106,7 @@ export default function CreateList() {
                   Clear List
                 </Button>
               </div>
-              <PlusBtn bgColor={"#8644A2"} clickOn={handleIsShow}>
-                &#43;
-              </PlusBtn>
+              <PlusBtn bgColor={"#8644A2"}>&#43;</PlusBtn>
             </div>
           </div>
         </form>
@@ -122,10 +127,12 @@ function List({ items }) {
 
 function ListItem({ item }) {
   return (
-    <li className="grid grid-cols-3 mx-3 rounded-lg bg-gray-300 divide-dotted divide-x-2 divide-darkViolet space-x-6 px-3 py-2">
+    <li className="grid grid-cols-5 mx-3 bg-gray-200 divide-dotted divide-x-2 divide-darkViolet space-x-6 px-3 py-2">
       <span>{item.name}</span>
       <span>{item.quantity}</span>
+      <span>{item.unit}</span>
       <span>{item.price}</span>
+      <span>+, -</span>
     </li>
   );
 }
@@ -136,20 +143,25 @@ function TitleCom() {
 
   function handleTitle(e) {
     e.preventDefault();
+    if (!title) return;
     setShowTitle((showTitle) => !showTitle);
   }
 
   return (
     <form>
-      <div className="flex justify-center mt-9 items-center space-x-4">
-        {showTitle && <h1 className="text-xl">{title}</h1>}
+      <div className="flex justify-between mx-3 mt-9 items-center space-x-4">
+        {showTitle && (
+          <h1 title="Title" className="md:text-2xl font-semibold capitalize ">
+            {title}
+          </h1>
+        )}
         {!showTitle && (
           <input
             value={title}
             onChange={(e) => setTitle(e.target.value)}
             type="text"
             placeholder="Enter your list name..."
-            className="px-2 w-3/4 text-lg py-2 mx-3 input-createlist"
+            className="px-2 w-4/5 text-lg py-2 mx-3 border border-gray-400 rounded-lg input-createlist"
           />
         )}
         <PlusBtn clickOn={handleTitle} bgColor={"#8644A2"}>
