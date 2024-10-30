ae82de2e069c09d94eb5a2963f7521a183056670
Mirwais <mirwais.sanaie@gmail.com>
2024-10-30 23:58:49 +0430

    About page started.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,15 @@ function About() {
   return (
     <div>
       <Navigation />
-      <h1 className="text-center font-black text-3xl">This is About us Page</h1>
+      <div className="container mx-auto text-center">
+        <h1 className="text-4xl text-darkViolet my-3 font-semibold">
+          About Us
+        </h1>
+        <div className="flex flex-col md:flex-row">
+          <img src="" alt="../../public/Mirwais.JPG" />
+          <div className="flex flex-col"></div>
+        </div>
+      </div>
     </div>
   );
 }
